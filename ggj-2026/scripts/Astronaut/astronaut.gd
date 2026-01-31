bc20a4e668c76d34dd734a65606dff6dce6d16e1
MmmGarlicFingers <120672054+MmmGarlicFingers@users.noreply.github.com>
2026-01-31 16:40:37 -0400

    Yeah

--- ggj-2026/scripts/Astronaut/astronaut.gd
+++ ggj-2026/scripts/Astronaut/astronaut.gd
@@ -2,14 +2,27 @@ extends CharacterBody2D
 
 var is_player = false
 var speed = 300
+var navmesh : NavigationMesh
 
 @onready var outline = $Outline
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
+func _physics_process(delta: float) -> void:
+	var direction = decide_move()
+
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.y = direction.y * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
+
+	move_and_slide()
 
 func set_hover(hover_status : bool) -> void:
 	outline.visible = hover_status
 
-func decide_move():
+func decide_move() -> Vector2:
 	pass
