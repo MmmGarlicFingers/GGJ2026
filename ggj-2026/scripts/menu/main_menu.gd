2e042a7b31b75e7c2212ee3e0e67f24e4291dd34
Camilo <camilosanpor@gmail.com>
2026-02-01 13:34:56 -0400

    awesome stuff

--- ggj-2026/scripts/menu/main_menu.gd
+++ ggj-2026/scripts/menu/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://prefabs/UI/close_your_eyes.tscn")
+	get_tree().change_scene_to_file("res://scenes/text_crawl.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
